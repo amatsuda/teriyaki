ed57ed817fce865a0b3bc7d8b2e068212dbecb79
Akira Matsuda <ronnie@dio.jp>
2010-09-14 14:04:26 +0900

    initial version of Teriyaki

---
+++ lib/teriyaki/steak.rb
@@ -0,0 +1,15 @@
+m = Module.new do
+  Rails.application.routes.routes.map {|r| [r.name, r.path.sub(/\(\.:format\)$/, '')]}.uniq.each do |name, path|
+    variables = path.scan(/:(.*?)(\/|$)/).map(&:first)
+    variables.each do |v|
+      path.sub! ":#{v}", '#{' + "#{v}.respond_to?(:to_key) ? #{v}.to_key : #{v}" + '}'
+    end
+    class_eval <<-ROUTE
+    def #{name}_path(#{variables.join(', ')})
+      "#{path}"
+    end
+ROUTE
+  end
+end
+
+Rspec.configuration.include(m)
